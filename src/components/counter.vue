fc31d3db51b18a5c8453b4ce11b579cec9ec17f0
sujunfei <1982764150@qq.com>
2016-07-20 15:06:40 +0800

    finish the calculator

--- src/components/counter.vue
+++ src/components/counter.vue
@@ -10,8 +10,8 @@
 			<button class="num_button" v-for="i in 9" @click="getNum($event)">{{i+1}}</button>
 		</div>
 		<div class="num">
-			<button v-for="operator in ['+','-','*','/','(',')']" class="num_button" @click="addOperator($event)">{{operator}}</button>
-			<button class="num_button" @click="backpace">B</button>
+			<button v-for="operator in ['+','-','*','/','(',')']" class="num_button" @click="getOperator($event)">{{operator}}</button>
+			<button class="num_button" @click="backspace">B</button>
 			<button class="num_button" @click="clearData">C</button>
 			<button class="num_button" @click="compute">=</button>
 		</div>
@@ -23,20 +23,12 @@
 	export default {
 		data() {     
 			return {
-				countValue: '',  //the equation for show
-				equation: [],  //the equation for translate
-				lateEquation: [],  //  the equation for compute
-				stack: [], 
-				operateStack: [],
-				continue: false,  //to joint the operate num 
-				priority : {
-					'+' : 2,
-					'-' : 2,
-					'(' : 0,
-					')' : 1,
-					'*' : 3,
-					'/' : 3
-				}
+				countValue: '',  // the equation for show
+				equation: [],  // the array for nifix expression
+				lateEquation: [],  //  the array for postfix expression
+				stack: [],  // the locaiotn for excute
+				operateStack: [], // the array for transform operator
+				continue: false  // to joint the operate num 
 			}
 		},
 		vuex: {
@@ -46,7 +38,8 @@
 			}
 		},
 		methods: {
-			getNum: function(event) {   //get the operate num value and push in num[]
+			// get the operate num value and push in num[]
+			getNum: function(event) {   
 				var value = event.target.innerHTML
 				this.countValue += value
 				if(this.continue){
@@ -56,18 +49,23 @@
 				}
 				this.continue = true
 			},
-			addOperator: function(event){  //get the operator value and push in operator[]
+			// get the operator value and push in operator[]
+			getOperator: function(event){ 
 				this.continue = false
 				this.countValue += event.target.innerHTML
 				this.equation.push(event.target.innerHTML)
 			},
-			backpace: function() {
+			// the function like the backspace 
+			backspace: function() {
 				let tmp = []
 				let len = this.equation.length
 				// reset the countValue
 				tmp = this.countValue.split('')
 				tmp.pop()
-				this.countValue = tmp.toString()
+				this.countValue = ''
+				for(var i = 0; i < tmp.length; i++) {
+					this.countValue += tmp[i]
+				}
 				// reset the equation
 				if(typeof (this.equation[len-1]) === 'number') {
 				 this.equation[len-1] / 5 < 2 ? this.equation.pop() : this.equation[len-1] = parseInt(this.equation[len-1] / 10) 
@@ -84,18 +82,18 @@
 				this.equation = []
 				this.lateEquation = []
 			},
-			compute: function() {  //excute the equation
-				let self = this
-				// if(self.countValue.match(/^[0-9]+([\+\-\*\/][0-9]+)+$/) !== null){  //judge the equation is valid or not
-					self.middleTolate(self.equation)
-					self.excute(self.lateEquation)
-					console.log(self.lateEquation)
-				// }else{
-				// 	console.log('the equation is not valid')
-				// }
+			// get the final result and check the expression is valid or not
+			compute: function() { 
+				if(this.countValue.match(/^[0-9]+([\+\-\*\/][0-9]+)+$/) !== null) { 
+					this.nifixTopostfix(this.equation)
+					console.log(this.lateEquation)
+					this.excute(this.lateEquation)
+				}else {
+					console.log('the equation is not valid')
+				}
 			},
 			//get the min index of 'x' and '/'
-			min: function([ a, b ] = [ 1, 1 ]){  
+			min: function([ a, b ] = [ 1, 1 ]) {  
 				let result = ''
 				a < 0 ? result = b : b < 0 ? result = a : a < b ? result = a : result = b
 				return result
@@ -111,16 +109,17 @@
 				return -1;
 			},
 			// transform the nifix expression to postfix expression
-			middleTolate: function(equation) {
-				var eLen = equation.length
+			nifixTopostfix: function(equation) {
+				let eLen = equation.length
+				let priority = { '+' : 2, '-' : 2, '(' : 0, ')' : 1, '*' : 3, '/' : 3 }
 				for (var i = 0 ; i < eLen; i++) {
 					if(typeof(equation[i]) === 'number') {  //the nummber push into lateEquation directly
 						this.lateEquation.push(equation[i])
 					}else{
 						if(this.operateStack.length !== 0 ) {  
-							if(this.priority[equation[i]] === 0) {
+							if(equation[i] === '(') {
 								this.operateStack.unshift(equation[i])  //if the operator === '(' push into operateStack directly
-							}else if(this.priority[equation[i]] === 1) {
+							}else if(priority[equation[i]] === 1) {
 									// push the operator in the front of '(' into lateEquation and delete it from operateStack
 									for(var l = 0, oLen =this.operateStack.length; l < oLen; l++) {
 										var shifted = this.operateStack.shift()
@@ -130,12 +129,12 @@
 									}
 							}else {
 								// if current operator's prioity over the first operator's prioity in operateStack push into operateStack directly
-								if(this.priority[equation[i]] >= this.priority[this.operateStack[0]]) {
+								if(priority[equation[i]] >= priority[this.operateStack[0]]) {
 									this.operateStack.unshift(equation[i])
 								}else {
 									//push the operator in operateStack that it's prioity higher than the current operator's prioity into lateEquation
 									for(var j = 0, oLen = this.operateStack.length ; j < oLen; j++) {
-										if(this.priority[this.operateStack[j]] >= this.priority[equation[i]]) { 
+										if(priority[this.operateStack[j]] >= priority[equation[i]]) { 
 											this.lateEquation.push(this.operateStack.shift())
 										}
 									}
@@ -152,6 +151,7 @@
 					this.lateEquation.push(this.operateStack.shift())
 				}
 			},
+			// to excute the postfix expression 
 			excute: function(equation) {
 				let type = {
 					'+' : this.increase,
